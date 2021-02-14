77433bfa49335c3da22f22b8cfa168d8ff44ea7e
evinracher <eevinley@gmail.com>
2021-02-14 18:57:25 -0500

    complete example

--- send_data/app.py
+++ send_data/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+
 @app.route('/on')
 def on():
-  return render_template('on.html')
+    return render_template('on.html')
+
+@app.route('/off')
+def off():
+    return render_template('off.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
