17e9ac3463001882f5187bf74be5a34a21230a08
evinracher <eevinley@gmail.com>
2021-02-14 22:22:30 -0500

    testing with text2emotion

--- send_data/app.py
+++ send_data/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import text2emotion as te
 # import RPi.GPIO as GPIO
 
 app = Flask(__name__)
@@ -6,6 +7,8 @@ app = Flask(__name__)
 
 @app.route('/on')
 def on():
+    text = "I was asked to sign a third party contract a week out from stay. If it wasn't an 8 person group that took a lot of wrangling I would have cancelled the booking straight away. Bathrooms - there are no stand alone bathrooms. Please consider this - you have to clear out the main bedroom to use that bathroom. Other option is you walk through a different bedroom to get to its en-suite. Signs all over the apartment - there are signs everywhere - some helpful - some telling you rules. Perhaps some people like this but It negatively affected our enjoyment of the accommodation. Stairs - lots of them - some had slightly bending wood which caused a minor injury."
+    print(te.get_emotion(text))
     return render_template('on.html')
 
 @app.route('/off')
