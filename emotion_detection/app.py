a9c6e5c169738e77db9d63cd330e77cf2c631b2c
evinracher <eevinley@gmail.com>
2021-03-26 18:48:51 -0500

    cleaning

--- emotion_detection/app.py
+++ emotion_detection/app.py
@@ -9,8 +9,24 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 translator = Translator()
 app = Flask(__name__)
 
+# TESTING PINS
+# Probably this is going to change, to run a different file
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+LED = 8
+
+GPIO.setup(LED, GPIO.OUT)
+
 def json_response(payload, status=200):
-    return (json.dumps(payload), status, {'content-type': 'application/json'})
+    return (json.dumps(payload), status,
+            {
+                'content-type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            })
 
 
 @app.route('/')
@@ -26,8 +42,10 @@ def process():
     print('Recieved from client: {}'.format(request.get_json(force=True)))
     msg = request.get_json()['msg']
     print(msg)
+    print("translating...")
     t_msg = translator.translate(msg)
     print(t_msg.text)
+    print("Processing...")
     emotion = t2e.get_emotion(t_msg.text)
     print(emotion)
     # emotionsArray = [('Happy', 6.0), ('Surprise', 5.0)]
@@ -41,12 +59,24 @@ def process():
     for (item, value) in emotionsArray:
         result.append(item)
     print(jsonify(result))
-    # res = jsonify(emotion)
-    return json_response(result)
-    # res = make_response(jsonify({"message": "El pepe"}), 200)
-    # print(res)
-    # return res
+    res = jsonify(emotion)
+    # working: return json_response(result)
+    res.headers.add("Access-Control-Allow-Origin", "*")
+    print(res)
+    return res
 
 
+@app.route('/led', methods=['GET'])
+@cross_origin()
+def switch():
+    state = GPIO.input(LED)
+
+    if state:
+        GPIO.output(LED, GPIO.LOW)
+        return "OFF"
+    else:
+        GPIO.output(LED, GPIO.HIGH)
+        return "ON"
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
