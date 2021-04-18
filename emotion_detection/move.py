bd54180317b4ba9a739a66b44ca626c4cbb90f29
evinracher <eevinley@gmail.com>
2021-04-18 18:06:48 -0500

    adding servo test

--- emotion_detection/move.py
+++ emotion_detection/move.py
@@ -1,15 +1,18 @@
 # MOVEMENT CODE
 import threading
-import time
+from time import sleep
 
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-# LED = 8
+LED = 8
+MOTOR = 33
 
-# GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(MOTOR, GPIO.OUT)
+GPIO.output(MOTOR, GPIO.LOW)
 
 class MovementControl(threading.Thread):
   emotions=[]
@@ -20,7 +23,28 @@ class MovementControl(threading.Thread):
   def run(self):
     count = 0
     print(self.emotions)
-    while count < 10:
-      time.sleep(1)
+    while count < 2:
+      sleep(1)
       count+=1
       print(count)
+    self.moveMotor(MOTOR,  50)
+
+  def moveMotor(self, motor, deg):
+    pwm=GPIO.PWM(motor, 50)
+    pwm.start(0)
+    pwm.ChangeDutyCycle(5) # left -90 deg position
+    sleep(0.5)
+    pwm.ChangeDutyCycle(7.5) # neutral position
+    sleep(0.5)
+    pwm.ChangeDutyCycle(10) # right +90 deg position
+    sleep(0.5)
+    pwm.stop()
+
+  # destructor
+  def __del__(self):
+    print("cleaning...")
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    print("Executing movement code")
+    MovementControl(["Neutral"]).start()
---
+++ emotion_detection/test_servo.py
@@ -0,0 +1,46 @@
+from gpiozero import Servo
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+MOTOR = 13
+
+GPIO.setup(MOTOR, GPIO.OUT)
+GPIO.output(MOTOR, GPIO.LOW)
+
+servo = Servo(MOTOR)
+val = -1
+S_TIME = 0.5
+
+# References:
+# https://www.learnrobotics.org/blog/raspberry-pi-servo-motor/
+# https://www.digikey.com/en/maker/blogs/2021/how-to-control-servo-motors-with-a-raspberry-pi
+
+
+def useAllValues():
+        global val
+        while True:
+                servo.value = val
+                sleep(0.1)
+                val = val + 0.1
+                if val > 1:
+                        val = -1
+
+def run():
+	while True:
+                servo.min()
+                sleep(S_TIME)
+                servo.mid()
+                sleep(S_TIME)
+                servo.max()
+                sleep(S_TIME)
+                        
+try:
+        run()
+except KeyboardInterrupt:
+        servo.min()
+        sleep(0.5)
+        print("Program stopped")
