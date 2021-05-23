e49a6c7a945cc0ca45802a82bd0f117636e6ae56
evinracher <eevinley@gmail.com>
2021-05-23 17:29:52 -0500

    mergin develop

--- emotion_detection/move.py
+++ emotion_detection/move.py
@@ -48,7 +48,6 @@ def getARandomElement(array):
 
 
 class MovementControl(threading.Thread):
-<<<<<<< HEAD
   emotions=[]
   stop = False
 
@@ -118,23 +117,3 @@ class MovementControl(threading.Thread):
 if __name__ == "__main__":
     print("Executing movement code")
     MovementControl(["Neutral"]).start()
-=======
-    emotions = []
-    stop = False
-    def __init__(self, emotions):
-        threading.Thread.__init__(self)
-        self.emotions = emotions
-
-    def run(self):
-        count = 0
-        print(self.emotions)
-        while count < 10:
-            if self.stop == True:
-              break
-            time.sleep(1)
-            count += 1
-            print(count)
-
-    def stop(self):
-        self.stop = True
->>>>>>> origin/develop
