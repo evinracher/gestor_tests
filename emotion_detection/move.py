75274f51c114717a2291ed295857bbed130297f2
evinracher <eevinley@gmail.com>
2021-06-02 22:13:57 -0500

    A little change

--- emotion_detection/move.py
+++ emotion_detection/move.py
@@ -12,15 +12,15 @@ HEAD_Y = 29
 HEAD_X = 31
 
 MOTORS = {
-  RIGHT_Z: "RIGHT_Z",
-  RIGHT_X: "RIGHT_X",
-  LEFT_Z: "LEFT_Z",
-  LEFT_X: "LEFT_X",
-  HEAD_Y: "HEAD_Y",
-  HEAD_X: "HEAD_X"
+    RIGHT_Z: "RIGHT_Z",
+    RIGHT_X: "RIGHT_X",
+    LEFT_Z: "LEFT_Z",
+    LEFT_X: "LEFT_X",
+    HEAD_Y: "HEAD_Y",
+    HEAD_X: "HEAD_X"
 }
 
-MOTOR_MIN = 0
+MOTOR_MIN = 23
 MOTOR_MID = 90
 MOTOR_MAX = 180
 
@@ -29,182 +29,171 @@ SLEEP_TIME = 1
 
 # move deg from 0 to 180
 MOVES_BY_EMOTION = {
-  "Neutral" : [
-    # gesture 1:
-    [
-#      { "motor": RIGHT_Z, "deg": 0, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 90, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 135, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 180, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 135, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 90, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 45, "wait": 0},
-#      { "motor": RIGHT_Z, "deg": 20, "wait": 1},
-      
-      { "motor": HEAD_X, "deg": 90, "wait": 0},
-#      { "motor": HEAD_X, "deg": 90, "wait": 0},
-#      { "motor": HEAD_X, "deg": 135, "wait": 0},
-#      { "motor": HEAD_X, "deg": 180, "wait": 0},
-#      { "motor": HEAD_X, "deg": 135, "wait": 0},
-#      { "motor": HEAD_X, "deg": 90, "wait": 0},
-#      { "motor": HEAD_X, "deg": 45, "wait": 0},
-#      { "motor": HEAD_X, "deg": 20, "wait": 1},
-  
-   #   { "motor": LEFT_Z, "deg": 90, "wait": 0},
-   #   { "motor": LEFT_Z, "deg": 0, "wait": 0},
-   #   { "motor": HEAD_X, "deg": 25, "wait": 0},
-   #   { "motor": HEAD_X, "deg": 0, "wait": 0},
-   #   { "motor": HEAD_X, "deg": 45, "wait": 0},
-   #   { "motor": HEAD_X, "deg": 0, "wait": 0},
+    "Neutral": [
+        # gesture 1:
+        [
+            {"motor": RIGHT_Z, "deg": 0, "wait": 0},
+            {"motor": RIGHT_Z, "deg": 90, "wait": 0},
+
+            {"motor": LEFT_Z, "deg": 90, "wait": 0},
+            {"motor": LEFT_Z, "deg": 0, "wait": 0},
+
+            {"motor": HEAD_X, "deg": 45, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 45, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 1},
+        ],
+        # gesture 2:
     ],
-    # gesture 2:
-  ],
-  "Fear" : [
-    # gesture 1:
-    [
-      { "motor": HEAD_Y, "deg": 180, "wait": 0},
-      { "motor": HEAD_Y, "deg": 0, "wait": 0},
-      { "motor": RIGHT_X, "deg": 180, "wait": 0},
-      { "motor": RIGHT_X, "deg": 0, "wait": 0},
-      { "motor": LEFT_X, "deg": 180, "wait": 0},
-      { "motor": LEFT_X, "deg": 0, "wait": 1},
+    "Fear": [
+        # gesture 1:
+        [
+            {"motor": HEAD_Y, "deg": 180, "wait": 0},
+            {"motor": HEAD_Y, "deg": 0, "wait": 0},
+            {"motor": RIGHT_X, "deg": 180, "wait": 0},
+            {"motor": RIGHT_X, "deg": 0, "wait": 0},
+            {"motor": LEFT_X, "deg": 180, "wait": 0},
+            {"motor": LEFT_X, "deg": 0, "wait": 1},
+        ],
+        # gesture 2:
     ],
-    # gesture 2:
-  ],
-  "Surprise" : [
-    # gesture 1:
-    [
-      { "motor": RIGHT_Z, "deg": 180, "wait": 0},
-      { "motor": RIGHT_Z, "deg": 0, "wait": 0},
-      { "motor": LEFT_Z, "deg": 180, "wait": 0},
-      { "motor": LEFT_Z, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 25, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 45, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
+    "Surprise": [
+        # gesture 1:
+        [
+            {"motor": RIGHT_Z, "deg": 180, "wait": 0},
+            {"motor": RIGHT_Z, "deg": 0, "wait": 0},
+            {"motor": LEFT_Z, "deg": 180, "wait": 0},
+            {"motor": LEFT_Z, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 25, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 45, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+        ],
+        # gesture 2:
     ],
-    # gesture 2:
-  ],
-  "Angry" : [
-    # gesture 1:
-    [
-      { "motor": RIGHT_X, "deg": 25, "wait": 0},
-      { "motor": RIGHT_X, "deg": 0, "wait": 0},
-      { "motor": LEFT_X, "deg": 25, "wait": 0},
-      { "motor": LEFT_X, "deg": 0, "wait": 0},
-      { "motor": HEAD_Y, "deg": 135, "wait": 0},
-      { "motor": HEAD_Y, "deg": 45, "wait": 0},
-      { "motor": HEAD_Y, "deg": 180, "wait": 0},
-      { "motor": HEAD_Y, "deg": 0, "wait": 0},
+    "Angry": [
+        # gesture 1:
+        [
+            {"motor": RIGHT_X, "deg": 25, "wait": 0},
+            {"motor": RIGHT_X, "deg": 0, "wait": 0},
+            {"motor": LEFT_X, "deg": 25, "wait": 0},
+            {"motor": LEFT_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_Y, "deg": 135, "wait": 0},
+            {"motor": HEAD_Y, "deg": 45, "wait": 0},
+            {"motor": HEAD_Y, "deg": 180, "wait": 0},
+            {"motor": HEAD_Y, "deg": 0, "wait": 0},
+        ],
+        # gesture 2:
     ],
-    # gesture 2:
-  ],
-  "Sad" : [
-    # gesture 1:
-    [
-      { "motor": RIGHT_Z, "deg": 45, "wait": 0},
-      { "motor": RIGHT_Z, "deg": 0, "wait": 0},
-      { "motor": LEFT_Z, "deg": 45, "wait": 0},
-      { "motor": LEFT_Z, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 25, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 25, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
+    "Sad": [
+        # gesture 1:
+        [
+            {"motor": RIGHT_Z, "deg": 45, "wait": 0},
+            {"motor": RIGHT_Z, "deg": 0, "wait": 0},
+            {"motor": LEFT_Z, "deg": 45, "wait": 0},
+            {"motor": LEFT_Z, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 25, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 25, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+        ],
+        # gesture 2:
     ],
-    # gesture 2:
-  ],
-  "Happy" : [
-    # gesture 1:
-    [
-      { "motor": RIGHT_X, "deg": 180, "wait": 0},
-      { "motor": RIGHT_X, "deg": 0, "wait": 0},
-      { "motor": LEFT_X, "deg": 180, "wait": 0},
-      { "motor": LEFT_X, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 45, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
-      { "motor": HEAD_X, "deg": 45, "wait": 0},
-      { "motor": HEAD_X, "deg": 0, "wait": 0},
-    ],
-    # gesture 2:
-  ]  
+    "Happy": [
+        # gesture 1:
+        [
+            {"motor": RIGHT_X, "deg": 180, "wait": 0},
+            {"motor": RIGHT_X, "deg": 0, "wait": 0},
+            {"motor": LEFT_X, "deg": 180, "wait": 0},
+            {"motor": LEFT_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 45, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+            {"motor": HEAD_X, "deg": 45, "wait": 0},
+            {"motor": HEAD_X, "deg": 0, "wait": 0},
+        ],
+        # gesture 2:
+    ]
 }
 
-def getARandomInt(from_num = 0, to_num = 10):
-  seed = str(round(time()))
-  seed = int(seed[len(seed)-1])/10
-  return round(seed * (to_num - from_num)) +  from_num
+
+def getARandomInt(from_num=0, to_num=10):
+    seed = str(round(time()))
+    seed = int(seed[len(seed)-1])/10
+    return round(seed * (to_num - from_num)) + from_num
+
 
 def getARandomElement(array):
-  index = getARandomInt(0, len(array) - 1)
-  return array[index] 
+    index = getARandomInt(0, len(array) - 1)
+    return array[index]
 
 
 class MovementControl(threading.Thread):
-  emotions=[]
-  stop = False
+    emotions = []
+    stop = False
 
-  def __init__(self, emotions):
-    threading.Thread.__init__(self)
-    self.emotions = emotions
- 
-  def run(self):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-    for motor in MOTORS.keys():
-      GPIO.setup(motor, GPIO.OUT)
-      GPIO.output(motor, GPIO.LOW)
-    print("starting movement")
-    self.movement()
-    return
+    def __init__(self, emotions):
+        threading.Thread.__init__(self)
+        self.emotions = emotions
 
-  def stop(self):
-    self.stop = True
+    def run(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        for motor in MOTORS.keys():
+            GPIO.setup(motor, GPIO.OUT)
+            GPIO.output(motor, GPIO.LOW)
+        print("starting movement")
+        self.movement()
+        return
 
-  def movement(self):
-    count = 0
-    if(len(self.emotions) == 0):
-      return
-    print("start inmediatly")
-    while count < 2:
-      if(self.stop == True):
-        break
-      emotion = getARandomElement(self.emotions)
-      print(emotion)
-      moves = getARandomElement(MOVES_BY_EMOTION[emotion])
-      for move in moves:
-        self.moveMotor(move["motor"], move["deg"])
-        # Put a delay here?
-        sleep(move["wait"])
-      count+=1
-    return
+    def stop(self):
+        self.stop = True
 
-  def moveMotor(self, motor, angle, time = SERVO_TIME):
-    pwm=GPIO.PWM(motor, 50)
-    pwm.start(0)
-    self.setAngle(motor, angle, time, pwm)
-    pwm.stop()
+    def movement(self):
+        count = 0
+        if(len(self.emotions) == 0):
+            return
+        print("start inmediatly")
+        while count < 2:
+            if(self.stop == True):
+                break
+            emotion = getARandomElement(self.emotions)
+            print(emotion)
+            moves = getARandomElement(MOVES_BY_EMOTION[emotion])
+            for move in moves:
+                self.moveMotor(move["motor"], move["deg"])
+                # Put a delay here?
+                sleep(move["wait"])
+            count += 1
+        return
 
-  def setAngle(self, motor, angle, time, pwm):
-    print("moving: " + MOTORS[motor] + ", " + str(angle) + " degrees" )
-    if angle < MOTOR_MIN:
-      angle = MOTOR_MIN
-    if angle > MOTOR_MAX:
-      angle = MOTOR_MAX
-    duty = angle / 18 + 2
-    GPIO.output(motor, GPIO.LOW)
-    pwm.ChangeDutyCycle(duty)
-    sleep(time)
-    GPIO.output(motor, GPIO.HIGH)
-    pwm.ChangeDutyCycle(duty)
-    sleep(time)
+    def moveMotor(self, motor, angle, time=SERVO_TIME):
+        pwm = GPIO.PWM(motor, 50)
+        pwm.start(0)
+        self.setAngle(motor, angle, time, pwm)
+        pwm.stop()
 
-  # destructor
-  def __del__(self):
-    print("cleaning...")
-    # TODO: Test the cleaning method with the real robot. We need to ensure that the secuence is correct.
-    for motor in MOTORS.keys():
-      self.moveMotor(motor, MOTOR_MIN)
-    GPIO.cleanup()
+    def setAngle(self, motor, angle, time, pwm):
+        print("moving: " + MOTORS[motor] + ", " + str(angle) + " degrees")
+        if angle < MOTOR_MIN:
+            angle = MOTOR_MIN
+        if angle > MOTOR_MAX:
+            angle = MOTOR_MAX
+        duty = angle / 18 + 2
+        GPIO.output(motor, GPIO.LOW)
+        pwm.ChangeDutyCycle(duty)
+        sleep(time)
+        GPIO.output(motor, GPIO.HIGH)
+        pwm.ChangeDutyCycle(duty)
+        sleep(time)
+
+    # destructor
+    def __del__(self):
+        print("cleaning...")
+        # TODO: Test the cleaning method with the real robot. We need to ensure that the secuence is correct.
+        for motor in MOTORS.keys():
+            self.moveMotor(motor, MOTOR_MIN)
+        GPIO.cleanup()
+
 
 if __name__ == "__main__":
     print("Executing movement code")
